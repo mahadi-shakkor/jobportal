86dd0e17958318ac77040474142f636d13f22552
mahadi-shakkor <shakkormahadi@gmail.com>
2026-07-24 00:38:27 +0600

    CORS - Browser’s Overprotective Parent

--- s5/jobportal/src/main/java/com/easybites/jobportal/company/controller/CompanyController.java
+++ s5/jobportal/src/main/java/com/easybites/jobportal/company/controller/CompanyController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/companies")
 @RequiredArgsConstructor
+//@CrossOrigin(origins = {"http://localhost:5173/"})
 public class CompanyController {
 
     private final ICompanyService companyService;
--- s5/jobportal/src/main/java/com/easybites/jobportal/web/WebConfig.java
+++ s5/jobportal/src/main/java/com/easybites/jobportal/web/WebConfig.java
@@ -39,29 +39,32 @@ public class WebConfig implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api",_ -> true);
     }
-//
-//    /**
-//     * Configure "global" cross-origin request processing. The configured CORS
-//     * mappings apply to annotated controllers, functional endpoints, and static
-//     * resources.
-//     * <p>Annotated controllers can further declare more fine-grained config via
-//     * {@link CrossOrigin @CrossOrigin}.
-//     * In such cases "global" CORS configuration declared here is
-//     * {@link CorsConfiguration#combine(CorsConfiguration) combined}
-//     * with local CORS configuration defined on a controller method.
-//     *
-//     * @param registry
-//     * @see CorsRegistry
-//     * @see CorsConfiguration#combine(CorsConfiguration)
-//     * @since 4.2
-//     */
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:5173")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .exposedHeaders("*")
-//                .allowCredentials(true).maxAge(3600);
-//    }
+
+    /**
+     * Configure "global" cross-origin request processing. The configured CORS
+     * mappings apply to annotated controllers, functional endpoints, and static
+     * resources.
+     * <p>Annotated controllers can further declare more fine-grained config via
+     * {@link CrossOrigin @CrossOrigin}.
+     * In such cases "global" CORS configuration declared here is
+     * {@link CorsConfiguration#combine(CorsConfiguration) combined}
+     * with local CORS configuration defined on a controller method.
+     *
+     * @param registry
+     * @see CorsRegistry
+     * @see CorsConfiguration#combine(CorsConfiguration)
+     * @since 4.2
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+
+
 }
